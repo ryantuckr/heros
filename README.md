8aa2d47d6292034e895b49baba7686782500d597
Ryan Tucker <ryantuckr@yahoo.com>
2017-12-19 13:03:04 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# heros
+# vetransConnect
+Team Project  due: 12-19
+
+3 beards and a Lady are:
+Alexa Rbinson,
+Karl Leroy,
+Ryan Tucker,
+Ryan Compton
+
+INSTRUCTIONS:
+
+Coding Requirements
+• Must use at least two APIs
+• Must use AJAX to pull data
+• Must utilize at least one new library or technology that we haven’t discussed
+• Must have a polished frontend / UI
+• Must meet good quality coding standards (indentation, scoping, naming)
+• Must NOT use alerts, confirms, or prompts (look into modals!)
+• Must have some sort of repeating element (table, columns, etc)
+• Must use Bootstrap or Alternative CSS Framework
+• Must be Deployed (Github Pages)
+• Must have User Input Validation 
+© 2016 | Coding Boot Camp - All Rights Reserved
+
+Coding – Nice To Haves
+• Utilize Firebase for Persistent Data Storage (Consider this basically a
+requirement).
+• Mobile Responsive
+• Use an alternative CSS framework like Materialize
+© 2016 | Coding Boot Camp - All Rights Reserved
+
+Presentation Requirement
+• You will also be responsible for preparing a 10 minute presentation.
+• This will be a formal presentation.
+• One in which you explain in detail:
+o Your overall application’s concept
+o The motivation for its development
+o Your design process
+o The technologies you used (and briefly how they work)
+o A demonstration of its functionality
+o Directions for future development
+• Treat the presentation seriously!
+• Talking intelligently about tech > doing tech sometimes. 
---
+++ assets/javascript/logic.js
@@ -0,0 +1,137 @@
+//when the 
+$(document).ready(function () {
+  // Parallax functionality
+  $('.parallax').parallax();
+  // carousel function
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+
+  // signout function
+  $("#logOutBtn").on("click", function (event) {
+    firebase.auth().signOut().then(function () {
+      console.log("User has signed out");
+    }).catch(function (error) {
+      // An error happened.
+      console.log("signout error");
+    });
+  });
+
+  //declaring token website to cature url
+  //for retrieving the Meet Up access token
+  var tokenWebsite = "";
+
+
+  //button for signing into Meet Up and getting access token
+  $("#meetBtn").on("click", function () {
+    console.log("button pressed");
+    //using oauth consumer key, send user to meet up
+    //so that we can get authorization to their account
+    //will redirect them to the landing.html page
+    window.location.replace("https://secure.meetup.com/oauth2/authorize?client_id=uslukvp5bbuco9nni5lgm900av&response_type=token&redirect_uri=https://meanderthal00.github.io/4ourheroes/landing.html");
+  });
+
+  if (window.location.href.includes("access_token")) {
+    tokenWebsite = window.location.href;
+    console.log("tokenWebsite conditional", tokenWebsite);
+    meetUpRequest();
+  }
+
+  //   // ajax function call for landing page ... meet-ups
+
+  function meetUpRequest() {
+    console.log("tokenWebsite:", tokenWebsite);
+    var token = new URL(tokenWebsite).hash.split('&').filter(function (el) {
+      if (el.match('access_token') !== null) return true;
+    });
+    console.log("token:", token);
+    //spliting the access token from the property title
+    var accessToken = token[0].split("=")[1];
+    console.log("accessToken:", accessToken);
+    $.ajax({
+      url: "https://api.meetup.com/find/upcoming_events",
+      method: 'GET',
+      data: {
+        page: 5,
+        access_token: accessToken,
+        key: "5a1b20747e54172335c4d412b296823",
+        sign: "true"
+      }
+    }).done(function (response) {
+      console.log(response);
+      response.events.forEach(renderMeets);
+
+    });
+  };
+
+  //ajax function for usajobs
+  $("#addLocation").on("click", function () {
+    console.log("submit pressed");
+    var locale = $("#locationInput").val().trim();
+    $("#locationInput").empty();
+    $.ajax({
+        url: `https://data.usajobs.gov/api/search?LocationName:${locale}&DatePosted:${30}`,
+        method: 'GET',
+        headers: {
+          'Authorization-Key': "hIa5Qx84CEfa6bI3BB2IVTBA30EYEYetV78R14xSuu4="
+        }
+      }).done(function (response) {
+        console.log(response);
+        response.SearchResult.SearchResultItems.forEach(renderJobs);
+      }),
+      function (error, response, body) {
+        var data = JSON.parse(body);
+      };
+  });
+
+
+
+  function renderJobs(element, index) {
+    console.log("success in rendering jobs function call");
+    var c = $("<div>");
+    c.addClass("newJob");
+    var title = element.MatchedObjectDescriptor.PositionTitle;
+    console.log(title);
+    var b = $(`<a>${title}</a>`);
+    c.append(b);
+    var link = element.MatchedObjectDescriptor.PositionURI;
+    console.log(link);
+    b.attr("href", link);
+    b.attr("target", "_blank");
+    var minPay = element.MatchedObjectDescriptor.PositionRemuneration[0].MinimumRange;
+    console.log(minPay);
+    c.append(`<p>Minimum Pay: ${minPay}</p>`);
+    $("#jobText").append(c);
+
+  }
+
+  function renderMeets(element, index) {
+    console.log("success in rendering meet-ups function call");
+    var d = $("<div>");
+    d.addClass("newMeet");
+    var title = element.name;
+    console.log(title);
+    var a = $(`<a>${title}</a>`);
+    d.append(a);
+    var group = element.group.name;
+    console.log(group);
+    d.append(`<br> ${group}<br>`);
+    var link = element.link;
+    console.log(link);
+    a.attr("href", link);
+    a.attr("target", "_blank");
+    var date = element.local_date;
+    d.append(`${date}<br>`);
+    var time = element.local_time;
+    console.log(date);
+    console.log(time);
+    d.append(time)
+    $("#meetText").append(d);
+
+
+  }
+
+
+});
