b77b72e1e834d5272154efc2b990421defea16b1
Ryan Tucker <ryantuckr@yahoo.com>
2017-12-19 13:55:52 -0500

    changes

---
+++ assets/javascript/resourcelogic.js
@@ -0,0 +1,23 @@
+var config = {
+    apiKey: "AIzaSyD9zUxSiYvAJ5aS_EhGLIx_MWILBbJy4TY",
+    authDomain: "project-1-530e9.firebaseapp.com",
+    databaseURL: "https://project-1-530e9.firebaseio.com",
+    projectId: "project-1-530e9",
+    storageBucket: "project-1-530e9.appspot.com",
+    messagingSenderId: "441047690869"
+  };
+
+
+  firebase.initializeApp(config);
+
+
+  // signout function
+$("#signOutBtn").on("click", function (event) {
+    firebase.auth().signOut().then(function () {
+      console.log("User has signed out");
+      window.location = 'index.html'; //After successful login, user will be redirected to index.html
+    }).catch(function (error) {
+      // An error happened.
+      console.log("signout error");
+    });
+  });
